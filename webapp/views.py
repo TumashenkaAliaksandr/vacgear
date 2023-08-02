840c21c18f626a0c6917d97515f4c57a7921a3b3
TumashenkaAliaksandr <Badminton500@inbox.lv>
2023-08-02 16:21:59 +0300

    hooked up the static

--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'webapp/index.html')
+
+
+def about(request):
+    return render(request, 'webapp/about.html')
