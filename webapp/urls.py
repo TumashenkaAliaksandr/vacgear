9e05b2718f7f7304fef124ae4a5af942cd60810f
TumashenkaAliaksandr <Badminton500@inbox.lv>
2023-08-05 21:00:05 +0300

    added new fuctions mediaroot - megia and added path for about

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,7 +1,16 @@
+from django.conf.urls.static import static
 from django.urls import path
+
+from vacgear import settings
 from webapp.views import *
+
+
+app_name = 'webapp'
 
 urlpatterns = [
     path('', index, name='home'),
     path('about/', about, name='about'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
